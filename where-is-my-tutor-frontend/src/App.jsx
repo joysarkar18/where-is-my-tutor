769091ca4858d8bf2897ee195379339679a4bbc2
Joy Sarkar <joysarkar8171@gmail.com>
2024-01-05 22:20:54 +0530

    update

--- where-is-my-tutor-frontend/src/App.jsx
+++ where-is-my-tutor-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import AppBar from './componants/appBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <AppBar></AppBar>
     <h1 className="text-5xl font-bold underline text-blue-700 bg-slate-800 h-screen w-screen text-center">
       where is my tutor
     </h1>
---
+++ where-is-my-tutor-frontend/src/componants/fuck.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function fuck() {
+  return (
+    <div>fuck</div>
+  )
+}
+
+export default fuck
