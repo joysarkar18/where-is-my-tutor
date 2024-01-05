8b4f173be367706fa000d3671d9e3e9e576f7d96
Stwarhatnibir <sanondodas@gmail.com>
2024-01-06 01:13:26 +0530

    navbar for welcomepage

--- where-is-my-tutor-frontend/src/App.jsx
+++ where-is-my-tutor-frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
+import Navbar from './navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-5xl font-bold underline text-blue-700 bg-slate-800 h-screen w-screen text-center">
-      where is my tutor
-    </h1>
+<Navbar/>
+<div className='bg-indigo-600 w-full h-screen'></div>
     </>
   )
 }
---
+++ where-is-my-tutor-frontend/src/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-black font-bold text-xl">Find My Tutor</div>
+        
+        <div className="flex space-x-6">
+          <a href="#" className="text-black">Home</a>
+          <a href="#" className="text-black">About</a>
+          <a href="#" className="text-black">Contact</a>
+
+          {/* Login Button */}
+          <button className="bg-white hover:bg-sky-700 text-black font-normal  py-1 px-8 rounded-full border border-sky-600">
+            Log In
+          </button>
+
+          {/* Signup Button */}
+          <button className="bg-sky-600 hover:bg-sky-700 text-white font-normal py-1 px-8 rounded-full">
+            Sign Up
+          </button>
+      
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
