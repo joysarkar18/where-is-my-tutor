16a3c8e1016315992ee93144ee572111d5618302
Joy Sarkar <joysarkar8171@gmail.com>
2024-01-07 17:19:53 +0530

    login api update

--- where-is-my-tutor-backend/routes/auth.js
+++ where-is-my-tutor-backend/routes/auth.js
@@ -28,133 +28,172 @@ router.post("/sign-up", [
 
         return res.status(400).json({ errors: errors.array() });
     }
-try {
+    try {
+
+        console.log(req.body.email, req.body.password);
+        const email = req.body.email;
+        const password = req.body.password;
+        const type = req.body.type;
+        const userName = req.body.userName;
+        if (userName == null || userName == undefined) {
+            return res.json({ status: false, error: { message: "plese provide userName in the body" } });
+        }
+
+        if (type === 0) {
+            let foundUser = await Student.findOne({ where: { email: email } });
+            if (foundUser != null) {
+                return res.json({ status: false, message: "User with this email already exists" });
+            }
+            let foundUser2 = await Student.findOne({ where: { userName: userName } });
+
+            if (foundUser2 != null) {
+                return res.json({ status: false, message: "User with this username already exists" });
+
+            }
+
+            else {
+
+                try {
+                    bcrypt.genSalt(saltRounds, function (err, salt) {
+                        bcrypt.hash(password, salt, function (err, hash) {
+                            // Store hash in database here
+
+                            Student.create({ email: email, password: hash, userName: userName }).then((result) => {
+
+                                console.log("student created in database");
+
+                                const payload = {
+                                    id: result.dataValues.id,
+                                    email: email,
+                                    userName: userName
+
+                                };
+
+                                let token = jwt.sign(payload, secret, options);
+
+                                return res.json({ status: true, message: "student account created", token: token })
+                            }).catch((err) => {
+                                return res.json({ status: false, error: err });
+                            })
+                        })
+                    });
+
+                } catch (error) {
+                    return res.json({ status: false, error: error });
+
+                }
+
+
+
+
+
+
+            }
+        }
+
+        if (type == 1) {
+
+            let foundUser = await Teacher.findOne({ where: { email: email } });
+            if (foundUser != null) {
+                return res.json({ status: false, message: "User with this email already exists" });
+            }
+            let foundUser2 = await Teacher.findOne({ where: { userName: userName } });
+
+            if (foundUser2 != null) {
+                return res.json({ status: false, message: "User with this username already exists" });
+
+            }
+
+            else {
+
+                try {
+                    bcrypt.genSalt(saltRounds, function (err, salt) {
+                        bcrypt.hash(password, salt, function (err, hash) {
+                            // Store hash in database here
+
+                            Teacher.create({ email: email, password: hash, userName: userName }).then((result) => {
+
+                                console.log("Teacher account created in database");
+
+                                const payload = {
+                                    id: result.dataValues.id,
+                                    email: email,
+                                    userName: userName
+
+                                };
+
+                                let token = jwt.sign(payload, secret, options);
+
+                                return res.json({ status: true, message: "Teacher account created", token: token })
+                            }).catch((err) => {
+                                return res.json({ status: false, error: err });
+                            })
+                        })
+                    });
+
+                } catch (error) {
+                    return res.json({ status: false, error: error });
+
+                }
+
+
+
+
+
+
+            }
+
+        }
+
+        res.json({ status: false, error: { message: "plese provide type in the body" } });
+
+    } catch (error) {
+
+        return res.json({ status: false, error: error });
+
+    }
+
+
+})
+
+router.post("/log-in" ,  async(req , res)=>{
 
     console.log(req.body.email, req.body.password);
     const email = req.body.email;
     const password = req.body.password;
-    const type = req.body.type;
     const userName = req.body.userName;
+    const type = req.body.type;
+
+    if(email==null || email==undefined){
+        let foundUser = await Student.findOne({ where: { userName: userName } });
+        if(foundUser!=null){
+            console.log(foundUser);
+            return;
+        }
+        res.json({found:"found"})
+
+    }
+
     if(userName==null || userName==undefined){
-       return res.json({status:false,  error: {message: "plese provide userName in the body"}});
-    }
-
-    if (type === 0) {
         let foundUser = await Student.findOne({ where: { email: email } });
-        if (foundUser != null) {
-            return res.json({ status: false, message: "User with this email already exists" });
+        if(foundUser!=null){
+            console.log(foundUser);
+            return;
         }
-        let foundUser2= await Student.findOne({ where: { userName: userName} });
 
-       if(foundUser2!=null){
-        return res.json({ status: false, message: "User with this username already exists" });
-
-       }
-
-        else {
-
-            try {
-                bcrypt.genSalt(saltRounds, function (err, salt) {
-                    bcrypt.hash(password, salt, function (err, hash) {
-                        // Store hash in database here
-
-                        Student.create({ email: email, password: hash, userName: userName }).then((result) => {
-
-                            console.log("student created in database");
-
-                            const payload = {
-                                id:result.dataValues.id,
-                                email: email,
-                                userName: userName
-
-                            };
-
-                            let token = jwt.sign(payload, secret, options);
-
-                            return res.json({ status: true, message: "student account created", token: token })
-                        }).catch((err) => {
-                            return res.json({status:false , error : err});
-                        })
-                    })
-                });
-
-            } catch (error) {
-                return res.json( {status:false , error: error});
-
-            }
-
-
-
-
-
-
-        }
-    }
-
-    if(type==1){
-
-        let foundUser = await Teacher.findOne({ where: { email: email } });
-        if (foundUser != null) {
-            return res.json({ status: false, message: "User with this email already exists" });
-        }
-        let foundUser2= await Teacher.findOne({ where: { userName: userName} });
-
-       if(foundUser2!=null){
-        return res.json({ status: false, message: "User with this username already exists" });
-
-       }
-
-        else {
-
-            try {
-                bcrypt.genSalt(saltRounds, function (err, salt) {
-                    bcrypt.hash(password, salt, function (err, hash) {
-                        // Store hash in database here
-
-                        Teacher.create({ email: email, password: hash, userName: userName }).then((result) => {
-
-                            console.log("Teacher account created in database");
-
-                            const payload = {
-                                id:result.dataValues.id,
-                                email: email,
-                                userName: userName
-
-                            };
-
-                            let token = jwt.sign(payload, secret, options);
-
-                            return res.json({ status: true, message: "Teacher account created", token: token })
-                        }).catch((err) => {
-                            return res.json({status:false , error : err});
-                        })
-                    })
-                });
-
-            } catch (error) {
-                return res.json( {status:false , error: error});
-
-            }
-
-
-
-
-
-
-        }
+        res.json({found:"found"})
 
     }
 
-    res.json({ status: false, error: {message: "plese provide type in the body"}});
-    
-} catch (error) {
+    res.json({error : "not found"})
 
-    return res.json( {status:false , error: error});
-    
-}
+
 
 
 })
+
+
+
 
 
 
