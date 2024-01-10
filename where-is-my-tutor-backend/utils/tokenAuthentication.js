6dcfd47b58a6a418b197645f2a530e8a019fdd53
Joy Sarkar <joysarkar8171@gmail.com>
2024-01-10 23:46:16 +0530

    middle ware update

--- where-is-my-tutor-backend/utils/tokenAuthentication.js
+++ where-is-my-tutor-backend/utils/tokenAuthentication.js
@@ -1,9 +1,19 @@
 const jwt = require("jsonwebtoken")
-
+const secret = 'please-fuck-yourself';
 const tokenAuthentication = (req , res , next)=>{
     const authHeader = req.headers['authorization']
-    console.log(authHeader);
-    next();
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token,secret , (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+    console.log(user);
+
+    next()});
     
 
 }
