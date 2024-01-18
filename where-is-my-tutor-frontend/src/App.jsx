09e3a5d71248d5f274e3c1d2e8466e08925aade2
Joy Sarkar <joysarkar8171@gmail.com>
2024-01-18 22:06:48 +0530

    update regarding the error for white screen

--- where-is-my-tutor-frontend/src/App.jsx
+++ where-is-my-tutor-frontend/src/App.jsx
@@ -1,38 +1,8 @@
-
-import React from 'react'
-import Navbar from './navbar'
-import { BrowserRouter as Router,Route,Switch,Link } from 'react-router-dom'
-import Login from './componants/login'
-
-
+import React from "react";
+import Navbar from "./navbar";
 
 function App() {
-
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            { <li>
-              <Link to="/">Navbar</Link>
-            </li> }
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          {<Route path="/">
-            <Navbar />
-          </Route> }
-        </Switch>
-      </div>
-    </Router>
-  );
+  return <Navbar></Navbar>;
 }
 
 export default App;
