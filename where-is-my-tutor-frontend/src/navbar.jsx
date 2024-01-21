562aa19be0656bf0bb7ff9f458a461c96bab4ec2
Joy Sarkar <joysarkar8171@gmail.com>
2024-01-21 19:39:39 +0530

    Navbar update

--- where-is-my-tutor-frontend/src/navbar.jsx
+++ where-is-my-tutor-frontend/src/navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-
-function Navbar  ()  {
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+function Navbar() {
   return (
-    <nav className="bg-white p-4">
+    <nav className="bg-white p-4 lg:mx-32 xl:mx-60">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-black font-bold text-xl">Find My Tutor</div>
+        <div className="block sm:hidden">
+          <GiHamburgerMenu className="h-[20px] w-[20px]" />
+        </div>
 
-        <div className="flex space-x-6">
-          <a href="#" className="text-black">Home</a>
-          <a href="#" className="text-black">About</a>
-          <a href="#" className="text-black">Contact</a>
+        <img className="h-10" src="src/assets/logo-no-background.png"></img>
+
+        <div className="flex space-x-4 sm:space-x-8 items-center">
+          <div className="hidden sm:flex space-x-8  items-center">
+            <a href="#" className="text-black">
+              Home
+            </a>
+            <a href="#" className="text-black">
+              About
+            </a>
+            <a href="#" className="text-black">
+              Contact
+            </a>
+          </div>
           {/* Login Button */}
-          <Link to="/Login"><button className="bg-white hover:bg-sky-700 text-black font-normal  py-1 px-8 rounded-full border border-sky-600">
-            Log In
-          </button></Link>
+          <Link to="/Login">
+            <button className="font-semibold text-baseColor-600 pt-[2.5px] pb-[3px]   px-6 rounded-md border border-baseColor-600">
+              Login
+            </button>
+          </Link>
           {/* Signup Button */}
-          <button className="bg-sky-600 hover:bg-sky-700 text-white font-normal py-1 px-8 rounded-full">
-            Sign Up
+          <button className="hover:bg-baseColor-400 font-semibold text-white py-1 px-6 rounded-md bg-baseColor-600">
+            Register
           </button>
         </div>
       </div>
--- where-is-my-tutor-frontend/tailwind.config.js
+++ where-is-my-tutor-frontend/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'baseColor-400': "#05B7BA",
+        "baseColor-600": "#028F94"
+      },
+    },
+
   },
   plugins: [],
 }
