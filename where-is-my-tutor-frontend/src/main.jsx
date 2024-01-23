dac38b69a57a4fe28fc2db41689e2394380ebf1d
Joy Sarkar <joysarkar8171@gmail.com>
2024-01-24 00:00:44 +0530

    update

--- where-is-my-tutor-frontend/src/main.jsx
+++ where-is-my-tutor-frontend/src/main.jsx
@@ -4,12 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./screens/login.jsx";
-<<<<<<< HEAD
 import Register from "./screens/register.jsx";
-=======
 import { Provider } from "react-redux";
 import findMyTutorStore from "./store/findMyTutorStore.js";
->>>>>>> 4f009e84bf416a070204cd6f4817c8f4c213f126
 
 const router = createBrowserRouter([
   {
