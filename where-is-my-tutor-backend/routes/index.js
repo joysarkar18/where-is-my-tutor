070ad596cea8f74e02237a78243e81035d15a9ca
Joy Sarkar <joysarkar8171@gmail.com>
2024-02-03 15:35:34 +0530

    update

--- where-is-my-tutor-backend/routes/index.js
+++ where-is-my-tutor-backend/routes/index.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send("hii from where is my tutor")
+
+
 });
 
 module.exports = router;
