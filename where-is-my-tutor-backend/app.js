d1d5c1307818d8be2ee3585348028363d489e9ad
Shreyo Paul <shreyopaul403@gmail.com>
2024-02-17 02:02:16 +0530

    media server ready!

--- where-is-my-tutor-backend/app.js
+++ where-is-my-tutor-backend/app.js
@@ -33,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/auth", authRouter);
 app.use('/', indexRouter);
